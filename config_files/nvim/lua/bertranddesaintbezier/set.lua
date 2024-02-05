9578a853068036f759403b43dfe6a10ee56f7f3f
Bertrand de Saint-Bezier <>
2024-02-05 19:45:28 +0100

    [more nvim config files + npm version manager]

--- config_files/nvim/lua/bertranddesaintbezier/set.lua
+++ config_files/nvim/lua/bertranddesaintbezier/set.lua
@@ -1,4 +1,3 @@
--- thicc vim cursor 
 vim.opt.guicursor = ""
 
 -- line & relative line numbers
@@ -7,6 +6,10 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 vim.opt.smartindent = true
 
 vim.opt.swapfile = false
+vim.opt.shadafile = "NONE"
